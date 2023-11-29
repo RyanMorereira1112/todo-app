664e1787767bc1421c10ba1d6cd01964873b201b
Ryan <ryanmoreira1112@gmail.com>
2023-11-29 15:13:05 -0300

    Salvando uma tarefa no banco de daods

--- index.js
+++ index.js
@@ -10,6 +10,33 @@ app.set('view engine' , 'handlebars')
 
 app.use(express.static('public'))
 
+
+//converter dados dos formulário em objeto
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+//rotas
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+        INSERT INTO tarefas(descricao, completa)
+        VALUES ('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
 })

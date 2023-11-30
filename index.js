5c1d2fe7e679ad73280bdd048f4bd51f321df9e2
Ryan <ryanmoreira1112@gmail.com>
2023-11-30 01:47:39 -0300

    Filtrando as tarefas ativas

--- index.js
+++ index.js
@@ -37,6 +37,24 @@ app.post('/completar', (requisicao, resposta) => {
     })
 })
 
+app.post('/descompletar', (requisicao, resposta) =>{
+   const id = requisicao.body.id
+   
+   const sql = `
+        UPDATE tarefas
+        SET completa = '0'
+        WHERE id= ${id}
+   `
+
+   conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+   })
+})
+
 app.post('/criar', (requisicao, resposta) => {
     const descricao = requisicao.body.descricao
     const completa = 0
@@ -70,7 +88,14 @@ app.get('/', (requisicao, resposta) => {
                 completa: dado.completa === 0 ? false : true
             }
         })
-        resposta.render('home', { tarefas })
+
+        const tarefasAtivas = tarefas.filter((tarefa) => {
+            return tarefa.completa === false && tarefa
+        })
+
+        const quantidadeTarefasAtivas = tarefasAtivas.length
+
+        resposta.render('home', { tarefas, quantidadeTarefasAtivas })
     })
 })
 
